a2a9710857c53211822fde2edd44aa26e5080c6a
danielmcc123 <danielmcc123123@gmail.com>
2018-10-18 16:54:03 +0100

    Added pagination to action point list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,20 +4,11 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule } from '@angular/router';
 
-
 import { AppRoutingModule } from './app.routing';
 import { ComponentsModule } from './components/components.module';
 
 import { AppComponent } from './app.component';
 
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
-import { TableListComponent } from './table-list/table-list.component';
-import { TypographyComponent } from './typography/typography.component';
-import { IconsComponent } from './icons/icons.component';
-import { MapsComponent } from './maps/maps.component';
-import { NotificationsComponent } from './notifications/notifications.component';
-import { UpgradeComponent } from './upgrade/upgrade.component';
 import {
   AgmCoreModule
 } from '@agm/core';
@@ -40,8 +31,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent,
-
+    AdminLayoutComponent
   ],
   providers: [WorkingGroupEndpointService],
   bootstrap: [AppComponent]
--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,7 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import * as Chartist from 'chartist';
-import {WorkingGroup, WorkingGroupEndpointService, ActionPointEndpointService, ActionPoint} from 'service';
+import {WorkingGroup, WorkingGroupEndpointService, ActionPoint} from 'service';
 import { Router, ActivatedRoute } from '@angular/router';
+import {MatPaginator, MatTableDataSource} from '@angular/material';
+
+export interface PeriodicElement {
+    name: string;
+    position: number;
+    weight: number;
+    symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+    {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+    {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+    {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+    {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+    {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+    {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+    {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+    {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+    {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+    {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 
 @Component({
   selector: 'app-dashboard',
@@ -9,9 +30,12 @@ import { Router, ActivatedRoute } from '@angular/router';
   styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
-
   workingGroups: WorkingGroup[];
   actionPoints: ActionPoint[];
+  displayedColumns: string[] = ['id', 'title', 'lastModified', 'leadContributor'];
+  dataSource = new MatTableDataSource(this.actionPoints);
+  @ViewChild(MatPaginator) paginator: MatPaginator;
+
 
   constructor(private router: Router, private workingGroupService: WorkingGroupEndpointService, private route: ActivatedRoute) { }
   startAnimationForLineChart(chart) {
@@ -50,9 +74,17 @@ export class DashboardComponent implements OnInit {
 
   listActionPoints(id: number) {
       this.workingGroupService.getActionPointsUsingGET1(id)
-          .subscribe(actionPoints => this.actionPoints = actionPoints.content)
+          .subscribe(actionPoints => {
+              this.actionPoints = actionPoints.content;
+              this.dataSource =  new MatTableDataSource(actionPoints.content);
+              this.dataSource.paginator = this.paginator;
+          })
   }
 
+
+  printId(id) {
+      console.log(id);
+  }
 
   startAnimationForBarChart(chart) {
       let seq2: any, delays2: any, durations2: any;
@@ -61,7 +93,7 @@ export class DashboardComponent implements OnInit {
       delays2 = 80;
       durations2 = 500;
       chart.on('draw', function(data) {
-        if(data.type === 'bar'){
+        if(data.type === 'bar')     {
             seq2++;
             data.element.animate({
               opacity: {
@@ -79,9 +111,8 @@ export class DashboardComponent implements OnInit {
   };
   ngOnInit() {
       /* ----------==========     Daily Sales Chart initialization For Documentation    ==========---------- */
-
       this.workingGroupService.getAllWorkingGroupsUsingGET().subscribe(workingGroups => this.workingGroups = workingGroups.content);
-
+      this.dataSource.paginator = this.paginator;
 
 
       const dataDailySalesChart: any = {
@@ -159,8 +190,10 @@ export class DashboardComponent implements OnInit {
       ];
       var websiteViewsChart = new Chartist.Bar('#websiteViewsChart', datawebsiteViewsChart, optionswebsiteViewsChart, responsiveOptions);
 
-      //start animation for the Emails Subscription Chart
+       // start animation for the Emails Subscription Chart
       this.startAnimationForBarChart(websiteViewsChart);
   }
 
 }
+
+
